d522b3d64eea72c91978ee03f5efed96518d8b61
munir <smsirajulmonir@gmail.com>
2025-01-08 18:13:51 +0600

    basic setup ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl bg-yellow-400'>Vite + React</h1>
+      <button className="btn btn-warning">Warning</button>
+
+    </div>
+  )
+}
+
+export default App
